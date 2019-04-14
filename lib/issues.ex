89d5513141c8c85482f49d3b4eb2725322538d51
shahidyousuf <shahidyousuf77@gmail.com>
2019-04-14 13:01:43 +0530

    - create project structure for issues app

---
+++ lib/issues.ex
@@ -0,0 +1,23 @@
+defmodule Issues do
+  @moduledoc """
+  Documentation for Issues.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Issues.hello()
+      :world
+
+  """
+  @author "Shahid Yousuf"
+  @profession "Software Developer"
+  @email "shahid.yousuf@trialx.com"
+  @company "TrialX | Applied Informatics Pvt. Ltd."
+  
+  def hello do
+    :world
+  end
+end
